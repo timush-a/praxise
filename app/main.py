ede46ad6b9f87ac8e2c139e49d6fc5ee5318785b
Timush Alexander <50115510+timush-a@users.noreply.github.com>
2021-03-20 22:49:40 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from server import Server
+from shelve_storage_driver import StorageDriver
 
 
 HOST = '127.0.0.1'
@@ -6,5 +7,7 @@ PORT = 5000
 
 
 if __name__ == '__main__':
-    local_server = Server(HOST, PORT)
+    db = StorageDriver('lite.csv')
+    db.load_storage()
+    local_server = Server(HOST, PORT, db, db.select)
     local_server.run_server()
